b96a2ce73de47c9264158b7e42ec8578fee040ff
muthita munmueang <muthita.mu.61@ubu.ac.th>
2020-09-24 17:52:03 +0700

    Updates

---
+++ precache-manifest.819f0452ecbea146da47e3b00b8ccb14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1afafb1951e6165fc069d45483e77d2",
+    "url": "/Translate/index.html"
+  },
+  {
+    "revision": "443143d3d59c2f87cf27",
+    "url": "/Translate/static/js/2.7cc903f1.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Translate/static/js/2.7cc903f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b538d3e1ae48f0d64c",
+    "url": "/Translate/static/js/main.09479afa.chunk.js"
+  },
+  {
+    "revision": "70ef58b36772c976b5cf",
+    "url": "/Translate/static/js/runtime-main.f7e16502.js"
+  }
+]);
